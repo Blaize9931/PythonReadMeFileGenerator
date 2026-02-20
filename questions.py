4da7edc15807b4cbb50de47aea35ca61b33590fd
Blaize9931 <blaize.kenny@outlook.com>
2026-02-20 14:12:43 +0000

    creating shell for questions

--- questions.py
+++ questions.py
@@ -1,2 +1,32 @@
 # Prompts the user for all specified details: title, description, installation, usage, license, author, and contact.
 from InquirerPy import prompt
+
+questions [
+    {
+        "name":"", "type": "", "message": "", "validate": ""
+    },
+    
+    {
+        "name":"", "type": "", "message": "", "validate": ""
+    },
+
+    {
+        "name":"", "type": "", "message": "", "validate": ""
+    },
+
+    {
+        "name":"", "type": "", "message": "", "validate": ""
+    },
+
+    {
+        "name":"", "type": "", "message": "", "validate": ""
+    },
+
+    {
+        "name":"", "type": "", "message": "", "validate": ""
+    },
+
+    {
+        "name":"", "type": "", "message": "", "validate": ""
+    }
+]
