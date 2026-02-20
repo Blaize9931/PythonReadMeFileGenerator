2ebb753f98ecc893b64689f843317db4364035fc
Blaize9931 <blaize.kenny@outlook.com>
2026-02-20 14:32:18 +0000

    Filled in questions array

--- questions.py
+++ questions.py
@@ -3,30 +3,30 @@ from InquirerPy import prompt
 
 questions [
     {
-        "name":"", "type": "", "message": "", "validate": ""
-    },
-    
-    {
-        "name":"", "type": "", "message": "", "validate": ""
+        "name":"title", "type": "input", "message": "Please provide the title of your Project"
     },
 
     {
-        "name":"", "type": "", "message": "", "validate": ""
+        "name":"description", "type": "editor", "message": "Please provide a description of the project", "validate": ""
     },
 
     {
-        "name":"", "type": "", "message": "", "validate": ""
+        "name":"installation", "type": "editor", "message": "What steps were taken to make this software runnable?", "validate": ""
     },
 
     {
-        "name":"", "type": "", "message": "", "validate": ""
+        "name":"usage", "type": "editor", "message": "How is this program designed to be used?", "validate": ""
     },
 
     {
-        "name":"", "type": "", "message": "", "validate": ""
+        "name":"license", "type": "expand", "message": "What licensing do you have?", "validate": ""
     },
 
     {
-        "name":"", "type": "", "message": "", "validate": ""
+        "name":"author", "type": "input", "message": "Please provide name/s of author", "validate": ""
+    },
+
+    {
+        "name":"Contact", "type": "input", "message": "Please give details of how to contact yourself.", "validate": ""
     }
 ]
