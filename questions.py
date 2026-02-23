e79c89befa524c803bb03e5b750a366da9ccafa7
Blaize9931 <blaize.kenny@outlook.com>
2026-02-23 15:41:20 +0000

    adding validation, and error handling

--- questions.py
+++ questions.py
@@ -7,23 +7,30 @@ def licensing_choices (_):
             ExpandChoice(key="m", name="MIT", value="MIT"),
             ExpandChoice(key="a", name="Apache2.0", value="Apache2.0"),
             ExpandChoice(key="b", name="BSD", value="BSD"),
-            ExpandChoice(key="t", name="The Unlicense", value="The Unlicense")
+            ExpandChoice(key="t", name="The Unlicense", value="The Unlicense"),
         ]
 questions = [
     {
-        "name":"title", "type": "input", "message": "Please provide the title of your Project"
+        "name":"title", "type": "input", "message": "Please provide the title of your Project", 
+        "validate":   lambda results: len(results) > 0,
+        "invalid_message": "Your title entry is blank, please give an answer", 
     },
 
     {
-        "name":"description", "type": "input", "message": "Please provide a description of the project"
+        "name":"description", "type": "input", "message": "Please provide a description of the project", "validate": lambda results: len(results) > 0,
+        "invalid_message": "Your description entry is blank, please give an answer",
     },
 
     {
-        "name":"installation", "type": "input", "message": "What steps were taken to make this software runnable?"
+        "name":"installation", "type": "input", "message": "What steps were taken to make this software runnable?",
+        "validate": lambda results: len(results) > 0,
+        "invalid_message": "Your installation entry is blank, please give an answer",
     },
 
     {
-        "name":"usage", "type": "input", "message": "How is this program designed to be used?"
+        "name":"usage", "type": "input", "message": "How is this program designed to be used?",
+        "validate": lambda results: len(results) > 0,
+        "invalid_message": "Your usage entry is blank, please give an answer",
     },
 
     {
@@ -35,7 +42,9 @@ questions = [
     },
 
     {
-        "name":"contact", "type": "input", "message": "Please give details of how to contact yourself." 
+        "name":"contact", "type": "input", "message": "Please give details of how to contact yourself.",
+        "validate": lambda results: len(results) > 0,
+        "invalid_message": "This field is required", 
     }
 ]
 def collect_answers():
