255059e5cdecce1e9e62fc95164998a4ebf35c22
Blaize9931 <blaize.kenny@outlook.com>
2026-02-24 17:13:00 +0000

    Wrote markdown format

--- markdown.py
+++ markdown.py
@@ -1,3 +1,31 @@
+import textwrap
+
 def markdown(results):
     title= results["title"]
-    return f"# {title}\n"
+    description= results["description"]
+    installation=results["installation"]
+    usage=results["usage"]
+    license=results["license"]
+    author=results["author"]
+    contact=results["contact"]
+    return textwrap.dedent(f"""\\
+    # {title}
+
+    ## Description
+    {description}
+
+    ## Installation
+    {installation}
+
+    ## Usage
+    {usage}
+
+    ## License
+    {license}
+
+    ## Author
+    {author}
+
+    ## Contact Details
+    {contact}
+    """)
