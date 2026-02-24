1fdc8b267af25f78cbf32f63905eb3d678a229a8
Blaize9931 <blaize.kenny@outlook.com>
2026-02-24 09:04:52 +0000

    connected markdown to main, and started markdown readme.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from questions import collect_answers
+from markdown import markdown
 results = collect_answers()
 print(results)
+md_content = markdown(results)
+
+print(md_content)
---
+++ markdown.py
@@ -0,0 +1,3 @@
+def markdown(results):
+    title= results["title"]
+    return f"# {title}\n"
